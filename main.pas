0c83e433ea454842aeb366d881ef469c3452c6d9
rime Art <Artrime@yeah.net>
2024-02-20 18:35:59 +0800

    做pagecontrol分页

--- main.pas
+++ main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  Registry;
+  ExtCtrls, Registry;
 
 type
 
@@ -14,8 +14,11 @@ type
 
   TMyForm = class(TForm)
     ListView1: TListView;
+    ListView2: TListView;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
     procedure FormCreate(Sender: TObject);
-    procedure ListBox1Click(Sender: TObject);
   private
 
   public
@@ -27,76 +30,112 @@ var
 
 implementation
 
+procedure PageLoadRegInList(PageControl: TPageControl);
+var
+  Reg: TRegistry;
+  RegKeyNames: TStringList;
+  RegKeyName, CurKey: string;
+  RegScans: array of string;
+  isSuccess: boolean;
+  isPageInit: array of boolean;
+  i, j, tabidx: integer;
+  CurListView: TListView;
+  CurListItem: TListItem;
+  CurRegRoot, CurRegPath: string;
+begin
+  RegScans := ['HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\',
+    'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\',
+    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'];
+
+  SetLength(isPageInit, Length(RegScans));
+  for i := 0 to Length(isPageInit) - 1 do isPageInit[i] := False;
+
+  for i := 0 to Length(RegScans) - 1 do
+  begin
+    Reg := TRegistry.Create;
+    RegKeyNames := TStringList.Create;
+
+    CurRegRoot := RegScans[i].Split(':')[0];
+    CurRegPath := RegScans[i].Split(':')[1].Substring(1);
+
+    { 设置根 }
+    case CurRegRoot of
+      'HKLM': begin
+        Reg.RootKey := HKEY_LOCAL_MACHINE;
+        tabidx := 0;
+      end;
+      'HKCU': begin
+        Reg.RootKey := HKEY_CURRENT_USER;
+        tabidx := 1;
+      end;
+    end;
+
+    if not isPageInit[tabidx] then
+    begin
+      PageControl.Pages[tabidx].Caption := CurRegRoot;
+      CurListView := (PageControl.Pages[tabidx].Controls[0] as TListView);
+      isPageInit[tabidx] := True;
+
+      // 添加列
+      CurListView.Columns.Add.Caption := 'DisplayName（显示名）';
+      CurListView.Columns.Add.Caption := 'Publisher（发布者）';
+      CurListView.Columns.Add.Caption := 'InstallDate（安装日期）';
+      CurListView.Columns.Add.Caption := 'UninstallString（卸载命令）';
+      CurListView.Columns.Add.Caption := 'Reg.KeyName（子键名）';
+      for j := 0 to CurListView.Columns.Count - 1 do
+        CurListView.Columns[j].Width := 200;
+      CurListView.Columns[j].Width := 400;
+    end;
+
+    try
+
+      { 打开指定的注册表键 }
+      if Reg.OpenKeyReadOnly(CurRegPath) then
+      begin
+        Reg.GetKeyNames(RegKeyNames); // 获取注册表键的子项名称
+        for RegKeyName in RegKeyNames do
+        begin
+          CurKey := CurRegPath + RegKeyName;
+          isSuccess := Reg.OpenKeyReadOnly(CurKey);
+
+          if True then
+          begin
+            if Reg.ValueExists('DisplayName') then
+            begin
+              CurListItem := CurListView.Items.Add;
+              CurListItem.Caption := Reg.ReadString('DisplayName');
+              CurListItem.SubItems.Add(Reg.ReadString('Publisher'));
+              CurListItem.SubItems.Add(Reg.ReadString('InstallDate'));
+              CurListItem.SubItems.Add(
+                Reg.ReadString('UninstallString'));
+              CurListItem.SubItems.Add(CurKey);
+            end;
+
+            Reg.CloseKey; // 关闭注册表键
+          end;
+
+        end;
+        Reg.CloseKey; // 关闭注册表键
+      end
+      else
+        ShowMessage('无法打开注册表键');
+    finally
+      Reg.Free; // 释放 TRegistry 对象
+      RegKeyNames.Free; // 释放 TStringList 对象
+    end;
+  end;
+end;
+
 {$R *.lfm}
 
 { TMyForm }
 
 procedure TMyForm.FormCreate(Sender: TObject);
-var
-  Reg: TRegistry;
-  RegKeys: TStringList;
-  RegKey, CurKey: string;
-  i: integer;
-  isSuccess: boolean;
-  CurListItem: TListItem;
 begin
 
-  // 添加列
-  ListView1.Columns.Add.Caption := 'DisplayName（显示名）';
-  ListView1.Columns.Add.Caption := 'Publisher（发布者）';
-  ListView1.Columns.Add.Caption := 'UninstallString（卸载命令）';
-  ListView1.Columns.Add.Caption := 'InstallDate（安装日期）';
-  ListView1.Columns.Add.Caption := 'Reg.KeyName（子键名）';
-  ListView1.Columns[0].Width := 200;
-  //ListView1.Columns[1].Width := 600;
-  for i := 1 to ListView1.Columns.Count - 1 do
-    ListView1.Columns[i].Width := 200;
-
-  Reg := TRegistry.Create;
-  RegKeys := TStringList.Create;
-  try
-    { 设置根 }
-    Reg.RootKey := HKEY_LOCAL_MACHINE;
-    { 打开指定的注册表键 }
-    if Reg.OpenKeyReadOnly('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\') then
-    begin
-      Reg.GetKeyNames(RegKeys); // 获取注册表键的子项名称
-      for RegKey in RegKeys do
-      begin
-
-        CurKey := 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\' + RegKey;
-        isSuccess := Reg.OpenKeyReadOnly(CurKey);
-
-        if True then
-        begin
-          if Reg.ValueExists('DisplayName') then
-          begin
-            CurListItem := ListView1.Items.Add;
-            CurListItem.Caption := Reg.ReadString('DisplayName');
-            CurListItem.SubItems.Add(Reg.ReadString('Publisher'));
-            CurListItem.SubItems.Add(Reg.ReadString('InstallDate'));
-            CurListItem.SubItems.Add(
-              Reg.ReadString('UninstallString'));
-            CurListItem.SubItems.Add(CurKey);
-          end;
-
-          Reg.CloseKey; // 关闭注册表键
-        end;
-
-      end;
-      Reg.CloseKey; // 关闭注册表键
-    end
-    else
-      ShowMessage('无法打开注册表键');
-  finally
-    Reg.Free; // 释放 TRegistry 对象
-    RegKeys.Free; // 释放 TStringList 对象
-  end;
-end;
-
-procedure TMyForm.ListBox1Click(Sender: TObject);
-begin
+  PageLoadRegInList(PageControl1);
 
 end;
+
 
 end.
